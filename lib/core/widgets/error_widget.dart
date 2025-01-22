82e182a0ce6580ac40f9e8829dcae89db7fee4fe
Hussein Awaisheh <h.awaesha97@gmail.com>
2025-01-22 17:00:18 +0300

    Modified PopScope - Exit App Dialog

--- lib/core/widgets/error_widget.dart
+++ lib/core/widgets/error_widget.dart
@@ -7,28 +7,30 @@ class CustomErrorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: .9,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.error,
-            color: Colors.grey.shade300,
-            size: 42,
-          ),
-          12.height,
-          Container(
-            color: Colors.black,
-            alignment: AlignmentDirectional.center,
-            child: Text('Something went wrong:\n${details.exception}',
-                style: TextStyle(
-                    color: Colors.grey.shade300,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w800),
-                textAlign: TextAlign.center),
-          ),
-        ],
+    return Material(
+      child: Transform.scale(
+        scale: .9,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error,
+              color: Colors.grey.shade300,
+              size: 42,
+            ),
+            12.height,
+            Container(
+              color: Colors.black,
+              alignment: AlignmentDirectional.center,
+              child: Text('Something went wrong:\n${details.exception}',
+                  style: TextStyle(
+                      color: Colors.grey.shade300,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.center),
+            ),
+          ],
+        ),
       ),
     );
   }
